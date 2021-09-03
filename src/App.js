d15e10bbfffb6ff49899381d40689ea185ed1323
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-03 11:19:17 -0400

    Connected landing page into App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import { useRecoilState } from 'recoil';
 import { websiteState } from './Recoil/atoms';
 import { Route } from 'react-router-dom'
-import { Button } from 'react-bootstrap';
+import LandingPage from './LandingPage/LandingPage'
 
 function App() {
   const [website, setWebite] = useRecoilState(websiteState)
 
-  return (
-    <div className="App">
-      <Button style={{ width: '50px', backgrundColor: 'black' }}> TEST </Button>
-    </div>
-  );
+  if (website === 'landing') {
+    return (
+      <div className="App">
+        <LandingPage />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1> Passed landing page</h1>
+      </div>
+    )
+  }
 }
 
 export default App;

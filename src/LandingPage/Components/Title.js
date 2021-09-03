31af759f3c5bc855bf2893131574120c2fa6f43f
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-03 12:27:36 -0400

    Completed rough layout of LandingPage.js

---
+++ src/LandingPage/Components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className='LandingTitleDiv'>
+            <h1 className='LandingTitleText'> La Bella's Salon </h1>
+        </div>
+    );
+};
+
+export default Title;

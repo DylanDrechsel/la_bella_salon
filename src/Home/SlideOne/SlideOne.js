71e8b793936f10daef9a9c1f44a83040ac7a8f2a
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-06 15:35:25 -0400

    Completed scrolling nav bar and set up Slide structure

---
+++ src/Home/SlideOne/SlideOne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Upper from './Upper/Upper'
+
+const SlideOne = () => {
+    return (
+        <div>
+            <Upper />
+        </div>
+    );
+};
+
+export default SlideOne;
---
+++ src/Home/SlideOne/Upper/Upper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Upper = () => {
+    return (
+        <div className='UpperDiv'>
+            <h1> Hello </h1>
+        </div>
+    );
+};
+
+export default Upper;

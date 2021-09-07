d96ba591fa5462b51c7799c3022a2c27499d4231
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-07 13:56:00 -0400

    Completed SalonEtiquette/Title.js and styling

--- src/SalonEtiquette/SalonEtiquette.js
+++ src/SalonEtiquette/SalonEtiquette.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './SalonEtiquette.css'
+import Title from './Components/Title'
 
 const SalonEtiquette = () => {
     return (
-        <div className='SalonEtiquetteDiv'>
+        <div className='EtiquetteMainDiv'>
+            <Title />
             
+            <div className='SalonEtiquetteDiv'>
+
+            </div>
         </div>
     );
 };

b2e89db173e303d20ef9e6def7c3069c0be21aaa
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-07 14:55:59 -0400

    Completed Cancellation.js

--- src/SalonEtiquette/Components/Arrival.js
+++ src/SalonEtiquette/Components/Arrival.js
@@ -5,9 +5,11 @@ const Arrival = () => {
         <div className='ArrivalDiv'>
             <h1 className='ArrivalTitle'> Arrival </h1>
             
-            <p className='ArrivalText'> 
+            <p className='ArrivalText'>
+                <b>
                 For Day Spa appointments please arrive 15 minutes prior to your scheduled services. 
                 If you arrive late we will make another attempt to accommodate you, however you may not be able to enjoy your Full Service Time. 
+                </b> 
             </p>
         </div>
     );
---
+++ src/SalonEtiquette/Components/Cancellations.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Cancellations = () => {
+    return (
+        <div className='CancellationsDiv'>
+            <h1 className='CancellationsTitle'> Cancellations </h1>
+            
+            <p className='CancellationsText'> 
+                <b>
+                    The sercices you Scheduled are reserved especially for you. Please advise us 24 Hours in advance, for multiple services 36 Hours in advance.
+                    A 50% deposit is required for multiple services in the Spa non-refundable if a courtesy cancellation call is not recieved.
+                </b>
+            </p>
+        </div>
+    );
+};
+
+export default Cancellations;

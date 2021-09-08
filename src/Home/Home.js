b6a8e9a19410a7429463f4fabb8a5e599ba45a77
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-08 15:13:57 -0400

    Navbar transparency is complete for home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,6 +3,10 @@ import './Home.css'
 import SlideOne from './SlideOne/SlideOne'
 
 const Home = () => {
+    document.addEventListener("scroll", () => {
+        document.documentElement.dataset.scroll = window.scrollY;
+      });
+
     return (
         <div>
             <SlideOne />

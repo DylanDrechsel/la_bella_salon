ec630dcbb6f54f95daeaba546ab65c7c4a0bc7a0
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-10 14:02:22 -0400

    Set up file structure for DropdownButton.js & MenuModal.js

--- src/Global/Navigation.js
+++ src/Global/Navigation.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import './global.css'
 import { Link } from 'react-router-dom'
 import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button, Dropdown } from 'react-bootstrap'
-import pic from '../Assets/transparent-la-bella-logo .png'
+// import pic from '../Assets/transparent-la-bella-logo .png'
 
 const Navigation = () => {
     const [navBackground, setNavBackground] = useState(false)

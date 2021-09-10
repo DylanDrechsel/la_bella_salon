5cb42e6d5523b09349d8a6d2cb04586d8a99fe9f
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-10 16:30:06 -0400

    Almost fully completed styling for a MenuModal.js

--- src/Global/MenuModal/MenuModal.js
+++ src/Global/MenuModal/MenuModal.js
@@ -1,28 +1,72 @@
 import React from 'react';
 import { Button, Modal } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const MenuModal = ({ handleShow, show, handleClose }) => {
     return (
         <>
             <Modal
+                className='Modal'
                 show={show}
                 onHide={handleClose}
-                backdrop="static"
+                // backdrop="static"
                 keyboard={false}
             >
-                <Modal.Header closeButton>
-                <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                I will not close if you click outside me. Don't even try to press
-                escape key.
+                {/* <Modal.Header closeButton >
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header> */}
+
+                <Modal.Body className='ModalBody'>
+
+                    <Link 
+                        to={'/'}
+                        onClick={handleClose}
+                        className='MenuLink'   
+                    >
+                        <h3 className='MenuText'> Home </h3> <br />
+                    </Link>
+
+                    <Link 
+                        to={'/salon'}
+                        onClick={handleClose}
+                        className='MenuLink'   
+                    >
+                        <h3 className='MenuText'> Salon </h3> <br />
+                    </Link>
+
+                    <Link 
+                        to={'/spa'}
+                        onClick={handleClose}
+                        className='MenuLink'   
+                    >
+                        <h3 className='MenuText'> Spa </h3> <br />
+                    </Link>
+
+                    <Link 
+                        to={'/salon-etiquette'}
+                        onClick={handleClose}
+                        className='MenuLink'
+                    >
+                        <h3 className='MenuText'> Salon Etiquette </h3> <br />
+                    </Link>
+
+                    <Link 
+                        to={'/team'}
+                        onClick={handleClose}
+                        className='MenuLink'
+                    >
+                        <h3 className='MenuText'> Team </h3> <br />
+                    </Link>
+
+                    <Link 
+                        to={'/contact'}
+                        onClick={handleClose}
+                        className='MenuLink'
+                    >
+                        <h3 className='MenuText'> Contact </h3> <br />
+                    </Link>
+
                 </Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary">Understood</Button>
-                </Modal.Footer>
             </Modal>
         </>
     );

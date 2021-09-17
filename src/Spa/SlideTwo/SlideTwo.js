5a414476160105222b5c53d58207077c77e8cf97
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-17 14:43:35 -0400

    Completed SlideTwo.js showing the information for 'Spa Package'

--- src/Spa/SlideTwo/SlideTwo.js
+++ src/Spa/SlideTwo/SlideTwo.js
@@ -3,7 +3,8 @@ import React from 'react';
 const SlideTwo = () => {
     return (
         <div className='SpaSlideTwoDiv'>
-            
+            <h1 className='SpaSildeTwoTitle'> <b> Spa Package! </b> </h1>
+            <p className='SpaSlideTwoText'> Choose 4 services and save 10%! </p>
         </div>
     );
 };

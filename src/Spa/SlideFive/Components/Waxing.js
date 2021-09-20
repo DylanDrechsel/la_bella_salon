73b3e99a67009f05dde2c34bbd92f2ccbb2564d9
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-20 11:46:51 -0400

    Completed Waxing Menu

--- src/Spa/SlideFive/Components/Waxing.js
+++ src/Spa/SlideFive/Components/Waxing.js
@@ -62,6 +62,133 @@ const Waxing = () => {
                         </p>
                     </Col>
                 </Row>
+            
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Brazilian </b>
+                        </p>
+                    </Col>
+
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $50 </b>
+                        </p>
+                    </Col>
+                </Row>
+            
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Legs (Full) </b>
+                        </p>
+                    </Col>
+
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $75 </b>
+                        </p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Legs (Upper or Lower) </b>
+                        </p>
+                    </Col>
+
+                    <Col xs={4} className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $48 </b>
+                        </p>
+                    </Col>
+                </Row>
+            
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Legs (Full with Bikini) </b>
+                        </p>
+                    </Col>
+
+                    <Col xs={4} className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $95 </b>
+                        </p>
+                    </Col>
+                </Row>
+            
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Back or Chest </b>
+                        </p>
+                    </Col>
+
+                    <Col xs={4} className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $48 </b>
+                        </p>
+                    </Col>
+                </Row>
+            
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Arms 1/2 </b>
+                        </p>
+                    </Col>
+
+                    <Col xs={4} className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $32 </b>
+                        </p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Arms (Full) </b>
+                        </p>
+                    </Col>
+
+                    <Col xs={4} className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $45 </b>
+                        </p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Underarm </b>
+                        </p>
+                    </Col>
+
+                    <Col xs={4} className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $28 </b>
+                        </p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className='WaxingCol'>
+                        <p className='WaxingTextLeft'>
+                            <b> Body Spot Wax </b>
+                        </p>
+                    </Col>
+
+                    <Col xs={4} className='WaxingCol'>
+                        <p className='WaxingTextRight'>
+                            <b> $18 </b>
+                        </p>
+                    </Col>
+                </Row>
+
             </div>
 
         </div>

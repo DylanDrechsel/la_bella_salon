9cd8cf8bdfad2514daeede3bf7a7d86d948d9c53
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-21 16:11:06 -0400

    Started working on SlideSeven.js which is going to be the Utlimate Spa Facial Special

---
+++ src/Spa/SlideSeven/SlideSeven.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SlideSeven = () => {
+    return (
+        <div className='SpaSlideSevenDiv'>
+            <h1 className='SpaSildeSevenTitle'> <b> Massage Menu! </b> </h1>
+        </div>
+    );
+};
+
+export default SlideSeven;

5f4ce44c13b5b986b4e78c8fc29f45216337f563
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-21 16:48:39 -0400

    Completed BodyMassageRelax.js for SlideEight.js"

---
+++ src/Spa/SlideEight/SlideEight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BodyMassageRelax from './Components/BodyMassageRelax';
+
+const SlideEight = () => {
+    return (
+        <div className='SpaSlideSevenDiv' style={{ paddingTop: '10px'}}>
+            <div className="SpaServiceDiv">
+                <BodyMassageRelax />
+            </div>
+        </div>
+    );
+};
+
+export default SlideEight;

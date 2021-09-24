753f37597b028e01dee0b5c66bd1e7ba3b443d0f
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-24 13:38:23 -0400

    Fixed CSS bugs for Iphone XR

--- src/Spa/SlideSeven/SlideSeven.js
+++ src/Spa/SlideSeven/SlideSeven.js
@@ -9,12 +9,12 @@ const SlideSeven = () => {
                 <p className='SpaSildeSevenText'> <b> Relax & Enjoy A Custom Facial with A Back ~~ Neck ~~ Shoulder Rub  </b> </p>
 
                 <Row>
-                    <Col xs={5}>
+                    <Col xs={7}>
                         <p className='SpaSildeSevenInfoText'> <b> ~1 Hr. 15 Min.~ </b> </p>
                     </Col>
 
-                    <Col xs={7}>
-                        <p className='SpaSildeSevenInfoText' style={{ paddingLeft: '150px'}}> <b> $125 </b> </p>
+                    <Col xs={3}>
+                        <p className='SpaSildeSevenInfoText' /* style={{ paddingLeft: '150px'}} */> <b> $125 </b> </p>
                     </Col>
                 </Row>
 
--- src/Spa/SlideThree/SlideThree.js
+++ src/Spa/SlideThree/SlideThree.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SpaRoom from '../../Assets/spa_room_la_bella.jpg'
 
 const SlideThree = () => {
     return (
         // Picture of Spa Room
         <div className='SpaSlideThreeDiv'>
-
+            <img className='SpaRoomPicture' src={SpaRoom} />
         </div>
     );
 };

4cbf30b37bbb7790a515864cdfb205c226503b94
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-27 14:19:45 -0400

    Completed SlideThirteen.js and Reflexology.js

---
+++ src/Spa/SlideThirteen/SlideThirteen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Reflexology from './Components/Reflexology';
+
+const SlideThirteen = () => {
+    return (
+        <div className='SpaSlideSevenDiv' style={{ paddingTop: '10px'}}>
+            <div className="SpaServiceDiv">
+                <Reflexology />
+            </div>
+        </div>
+    );
+};
+
+export default SlideThirteen;

d08185384aa0a4d0a14148357ee0172cbd205e67
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-27 15:34:29 -0400

    Css changes to ContactButton.js

--- src/Footer/Components/ContactButton.js
+++ src/Footer/Components/ContactButton.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap'
 const ContactButton = () => {
     return (
         <div className='ContactButtonDiv'>
-            <Button size="lg" variant="dark" href='tel:9082360866'> 908-236-0866 </Button>
+            <Button className='ContactButton' size="lg" variant="dark" href='tel:9082360866'> <h4 className='ContactButtonText'> 908-236-0866 </h4> </Button>
         </div>
     );
 };

889792e197275700cfdb81360b826cfffe986635
Dylan Drechsel <drechseldylan@gmail.com>
2021-09-28 11:40:46 -0400

    Started working on SlideThree.js for Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Footer.css'
 import SlideOne from './SlideOne/SlideOne';
 import SlideTwo from './SlideOne/SlideTwo';
+import SlideThree from './SlideThree/SlideThree';
 
 const Footer = () => {
     return (
         <div>
             <SlideOne />
             <SlideTwo />
+            <SlideThree />
         </div>
     );
 };

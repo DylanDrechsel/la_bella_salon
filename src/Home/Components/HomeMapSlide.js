492126aff1e648737d2a1c736d903320af04b2cb
Dylan Drechsel <drechseldylan@gmail.com>
2021-10-14 15:18:03 -0400

    Fixed styling for mapbox map

--- src/Home/Components/HomeMapSlide.js
+++ src/Home/Components/HomeMapSlide.js
@@ -3,19 +3,23 @@ import ReactMapGL from 'react-map-gl'
 
 const HomeMapSlide = () => {
     const [viewport, setViewport] = useState({
-        latitude: 40.6438102722168,
-        longitude: -74.82865905761719,
+        latitude: 40.64381057407044,
+        longitude: -74.82595317784701,
         width: '100vw',
         height: '50vh',
-        zoom: 15
+        zoom: 18
     })
+
+    console.log(viewport)
 
     return (
         <div>
             <ReactMapGL
                 {...viewport}
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                onViewportChange={nextViewport => setViewport(nextViewport)}>
+                onViewportChange={nextViewport => setViewport(nextViewport)}
+                mapStyle='mapbox://styles/dylandrechsel/ckm3pch0w220218qt4l0ud8yb'
+                >
                 markers here
             </ReactMapGL>
         </div>

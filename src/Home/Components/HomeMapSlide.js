62fdb604d4c539623517f8ac255863da12fb51a2
Dylan Drechsel <drechseldylan@gmail.com>
2021-10-14 16:00:45 -0400

    Completed map slide from Home.js

--- src/Home/Components/HomeMapSlide.js
+++ src/Home/Components/HomeMapSlide.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { Marker } from 'react-map-gl'
+import MapPin from '../../Assets/map-pin.png'
 
+const SIZE = 20;
+const UNIT = "px";
+  
 const HomeMapSlide = () => {
     const [viewport, setViewport] = useState({
         latitude: 40.64381057407044,
@@ -9,8 +13,8 @@ const HomeMapSlide = () => {
         height: '50vh',
         zoom: 18
     })
+    
 
-    console.log(viewport)
 
     return (
         <div>
@@ -20,7 +24,15 @@ const HomeMapSlide = () => {
                 onViewportChange={nextViewport => setViewport(nextViewport)}
                 mapStyle='mapbox://styles/dylandrechsel/ckm3pch0w220218qt4l0ud8yb'
                 >
-                markers here
+        
+                <Marker latitude={40.64385057407044} longitude={-74.82595317784701} offsetLeft={-20} offsetTop={-10} >
+                    <div> <p className='marker'> <b> La Bella Salon & Spa </b> </p> </div>
+
+                    {viewport.zoom > 18 ?
+                        <img style={{transform: `translate(${SIZE/2 + UNIT}, ${SIZE/2 + UNIT}`}} className='map-pin' src={MapPin} />
+                    : null }
+                </Marker>
+
             </ReactMapGL>
         </div>
     );

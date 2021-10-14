14d5e5a82bdbc20f6f3e02a6b80e07057b2c135f
Dylan Drechsel <drechseldylan@gmail.com>
2021-10-14 16:25:39 -0400

    Completed HomeDescriptionSlide.js

--- src/Footer/SlideOne/SlideTwo.js
+++ src/Footer/SlideOne/SlideTwo.js
@@ -6,8 +6,8 @@ const SlideTwo = () => {
     return (
         <div className='FooterTwoDiv'>
             <h1 className='FooterTwoText'> <b> To book an appointment at La Bella Salon & Spa, please give us a call, or visit our team page and contact your favorite stylist. </b> </h1>
-            <ContactButton />
             <TeamButton />
+            <ContactButton />
         </div>
     );
 };

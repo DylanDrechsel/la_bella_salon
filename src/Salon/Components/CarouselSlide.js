78cc53e00fea8a8a1cd532fe0c8a8fdd38ebbd78
Dylan Drechsel <drechseldylan@gmail.com>
2021-10-20 15:19:52 -0400

    changed jpg to pgn

--- src/Salon/Components/CarouselSlide.js
+++ src/Salon/Components/CarouselSlide.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Carousel } from 'react-bootstrap'
 import FirstPicture from '../../assets/carousel-one.jpg'
 import SecondPicture from '../../assets/carousel-two.jpg'
-// import ThirdPicture from '../../assets/carousel-three.jpg'
+import ThirdPicture from '../../assets/carousel-three.jpg'
 
 const CarouselSlide = () => {
     return (
@@ -24,13 +24,13 @@ const CarouselSlide = () => {
                     />
                 </Carousel.Item>
 
-                {/* <Carousel.Item>
+                <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={ThirdPicture}
                         alt="First slide"
                     />
-                </Carousel.Item> */}
+                </Carousel.Item>
 
             </Carousel>
         </div>
